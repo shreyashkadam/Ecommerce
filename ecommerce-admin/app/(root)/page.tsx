41db1db6fff4b1d804bcf4064f0a4beb97b9c46b
Shreyash Kadam <shreyash.kadam10@gmail.com>
2023-07-02 18:32:55 +0530

    Configure project for shadcn-ui

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <p>Hello Admin Dashboard</p>
+    <div className="p-4">
+      <Button>Click Me</Button>
+    </div>
   )
 }
