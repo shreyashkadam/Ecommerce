3c2f957c3fb7e0f703316c73e69733c666d7690a
Shreyash Kadam <shreyash.kadam10@gmail.com>
2023-07-09 16:34:14 +0530

    Add store switcher component to Navbar

--- ecommerce-admin/components/navbar.tsx
+++ ecommerce-admin/components/navbar.tsx
@@ -1,5 +1,7 @@
 import { UserButton, auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+
+import StoreSwitcher from "@/components/store-switcher";
 import { MainNav } from "@/components/main-nav";
 import prismadb from "@/lib/prismadb";
 
@@ -19,7 +21,7 @@ const Navbar = async () => {
   return ( 
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <div>This will be a store switcher</div>
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
